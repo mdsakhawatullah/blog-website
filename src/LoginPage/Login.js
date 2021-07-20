52ad0d23f191bea621f7e3c3ff21078a41aeb6dc
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2021-07-21 01:17:49 +0600

    register and login part complete

---
+++ src/LoginPage/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className="login">
+            <span className="loginTitle">Login</span>
+            <form className="loginForm">
+                <label htmlFor="">Email</label>
+                <input type="text" className="loginInput" placeholder="Enter your email" />
+                <label htmlFor="">Password</label>
+                <input type="password" className="loginInput" placeholder="Enter your password" />
+                <button className="loginButton">Login</button>
+            </form>
+            <button className="loginRegisterButton">Register</button>
+            
+        </div>
+    );
+};
+
+export default Login;
